7ece1852fc960b694f22876634ad24e7cfc23032
CarmenDominguez-at-104039976716 <carmengdominguez94@gmail.com>
2023-02-05 18:36:40 +0200

    added sass

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom";
 import App from "./components/App";
 
+import "./styles/global.css";
+
 const container = document.getElementById("root");
 const root = createRoot(container as Element);
 
