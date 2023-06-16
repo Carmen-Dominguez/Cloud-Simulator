9aaa9ba2f95984d8caf806f41c1653bf0160ead4
CarmenDominguez-at-104039976716 <carmengdominguez94@gmail.com>
2023-06-17 00:49:29 +0200

    day loop

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,14 +1,29 @@
-import React, { Reducer, useReducer } from "react";
+import React, { Reducer, useReducer, useState } from "react";
 import { Cloud } from "../Cloud";
 import { Sky } from "../Sky";
-import { reducer, AppState } from "../../reducer";
+import { reducer, AppState, TimeEvent } from "../../reducer";
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, "day");
+  const [phase, setPhase] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    TimeState: "day",
+    WeatherState: "clear",
+  } as AppState);
+
+  function nextPhase() {
+    dispatch({ TimePhase: true, NextDay: phase === 3 } as TimeEvent);
+    setPhase(phase === 3 ? 0 : phase + 1);
+  }
+
+  // setInterval(() => {
+  //   nextPhase();
+  // }, 5000);
 
   return (
-    <Sky time={state}>
-      <Cloud seedNumber={7} numOctaves={5} time={state} />
-    </Sky>
+    <div onClick={nextPhase}>
+      <Sky time={state.TimeState}>
+        <Cloud seedNumber={7} numOctaves={5} time={state.TimeState} />
+      </Sky>
+    </div>
   );
 }
--- src/reducer/index.tsx
+++ src/reducer/index.tsx
@@ -2,39 +2,41 @@ import React, { Reducer } from "react";
 
 export type AppState = {
   TimeState: "day" | "twilight" | "night";
-  WeatherState: "Clear" | "Cloudy" | "Rain" | "Storm";
+  WeatherState: "clear" | "cloudy" | "rain" | "storm";
 };
 
 export type TimeEvent = {
   TimePhase: boolean; // when time moves forward to the next phase
+  NextDay: boolean; // to signify the a new day
 };
 
 export const reducer: Reducer<AppState, TimeEvent> = (
   state: AppState,
   event: TimeEvent
 ) => {
-  let time: AppState["TimeState"] = "day";
-  let weather: AppState["WeatherState"] = "Clear";
+  let TimeState: AppState["TimeState"] = "day";
+  let WeatherState: AppState["WeatherState"] = "clear";
+
+  console.log(state, event);
 
   switch (state.TimeState) {
     case "day":
-      if (event.TimePhase) {
-        time = "twilight";
-      } else time = "day";
+      if (event.TimePhase) TimeState = "twilight";
       break;
 
     case "twilight":
-      if (event.TimePhase) {
-        time = "night";
-      } else time = "twilight";
+      if (event.TimePhase && event.NextDay) {
+        TimeState = "day";
+      } else TimeState = "night";
       break;
 
     case "night":
-      if (event.TimePhase) {
-        time = "twilight";
-      } else time = "night";
+      if (event.TimePhase) TimeState = "twilight";
       break;
+
+    default:
+      TimeState = "day";
   }
 
-  return { time, weather } as unknown as AppState;
+  return { TimeState, WeatherState } as unknown as AppState;
 };
