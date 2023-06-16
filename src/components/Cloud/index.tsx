ef6ac8a98ae28112a8d131c4de8357667c40c79c
CarmenDominguez-at-104039976716 <carmengdominguez94@gmail.com>
2023-06-16 22:45:48 +0200

    wip: cloud randomizer

--- src/components/Cloud/index.tsx
+++ src/components/Cloud/index.tsx
@@ -7,40 +7,44 @@ type Props = {
   time: "day" | "twilight" | "night";
 };
 
-export const Cloud = ({ seedNumber, numOctaves, time }: Props) => (
-  <div className={styles[time]}>
-    <div className={`${styles.cloud} ${styles.cloudBack}`}></div>
-    <div className={`${styles.cloud} ${styles.cloudMid}`}></div>
-    <div className={`${styles.cloud} ${styles.cloudFront}`}></div>
+export const Cloud = ({ seedNumber, numOctaves, time }: Props) => {
+  const randomiser = () => {};
 
-    <svg width="0" height="0">
-      <filter id="filter-back">
-        <feTurbulence
-          type="fractalNoise"
-          baseFrequency="0.012"
-          numOctaves={numOctaves}
-          seed={seedNumber}
-        />
-        <feDisplacementMap in="SourceGraphic" scale="170" />
-      </filter>
-      <filter id="filter-mid">
-        <feTurbulence
-          type="fractalNoise"
-          baseFrequency="0.012"
-          numOctaves={numOctaves}
-          seed={seedNumber}
-        />
-        <feDisplacementMap in="SourceGraphic" scale="150" />
-      </filter>
-      <filter id="filter-front">
-        <feTurbulence
-          type="fractalNoise"
-          baseFrequency="0.012"
-          numOctaves={numOctaves}
-          seed={seedNumber}
-        />
-        <feDisplacementMap in="SourceGraphic" scale="100" />
-      </filter>
-    </svg>
-  </div>
-);
+  return (
+    <div className={styles[time]}>
+      <div className={`${styles.cloud} ${styles.cloudBack}`}></div>
+      <div className={`${styles.cloud} ${styles.cloudMid}`}></div>
+      <div className={`${styles.cloud} ${styles.cloudFront}`}></div>
+
+      <svg width="0" height="0">
+        <filter id="filter-back">
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="0.012"
+            numOctaves={numOctaves}
+            seed={seedNumber}
+          />
+          <feDisplacementMap in="SourceGraphic" scale="170" />
+        </filter>
+        <filter id="filter-mid">
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="0.012"
+            numOctaves={numOctaves}
+            seed={seedNumber}
+          />
+          <feDisplacementMap in="SourceGraphic" scale="150" />
+        </filter>
+        <filter id="filter-front">
+          <feTurbulence
+            type="fractalNoise"
+            baseFrequency="0.012"
+            numOctaves={numOctaves}
+            seed={seedNumber}
+          />
+          <feDisplacementMap in="SourceGraphic" scale="130" />
+        </filter>
+      </svg>
+    </div>
+  );
+};
