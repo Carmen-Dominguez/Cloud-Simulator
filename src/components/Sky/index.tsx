789496271d5ae02e906e749265a84f9808f47aef
Carmen Dominguez <carmen.dominguez@platform45.com>
2024-10-01 14:00:18 +0200

    timePhase transition

--- src/components/Sky/index.tsx
+++ src/components/Sky/index.tsx
@@ -8,5 +8,11 @@ type Props = {
 };
 
 export const Sky: FC<Props> = ({ time, children }: Props) => (
-  <div className={`${styles.container} ${styles[time]}`}>{children}</div>
+  <div className={`${styles.container}`}>
+    <div style={{ opacity: time === 'day'? 1: 0 }} className={`${styles.day}`}></div>
+    <div style={{ opacity: time === 'dusk'? 1: 0 }} className={`${styles.dusk}`}></div>
+    <div style={time === 'night'? { opacity: 1}: { opacity: 0, backgroundPosition: 'center center' }} className={`${styles.night}`}></div>
+    <div style={{ opacity: time === 'dawn'? 1: 0 }} className={`${styles.dawn}`}></div>
+    {children}
+  </div>
 );
