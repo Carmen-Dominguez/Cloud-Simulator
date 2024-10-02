1ae816e5169b36994fc0d5bc51cf382388d241b5
Carmen Dominguez <carmen.dominguez@platform45.com>
2024-10-02 16:51:14 +0200

    refactor reducer

--- src/reducer/index.tsx
+++ src/reducer/index.tsx
@@ -5,36 +5,42 @@ export const reducer: Reducer<AppState, TimeEvent> = (
   state: AppState,
   event: TimeEvent
 ) => {
-  console.log('event', event);
   let TimeState: AppState["TimeState"] = "day";
   let WeatherState: AppState["WeatherState"] = "clear";
   let Timer: AppState["Timer"] = 10;
   let TimeEventTimes: AppState["TimeEventTimes"] = emptyTimeEventTimes;
 
-  switch (state.TimeState) {
-    case "day":
-      if (event.TimePhase) TimeState = "dusk";
-      break;
+  if (event.TimePhase) {
+    switch (state.TimeState) {
+      case "day":
+        TimeState = "dusk";
+        break;
 
-    case "dusk":
-      if (event.TimePhase) TimeState = "night";
-      break;
+      case "dusk":
+        TimeState = "night";
+        break;
 
-    case "night":
-      if (event.TimePhase) TimeState = "dawn";
-      break;
+      case "night":
+        TimeState = "dawn";
+        break;
 
-    case "dawn":
-      if (event.TimePhase) TimeState = "day";
-      break;
+      case "dawn":
+        TimeState = "day";
+        break;
 
-    default:
-      TimeState = "day";
+      default:
+        TimeState = "day";
+    }
   }
 
   if (event.TimeEventTimes) TimeEventTimes = event.TimeEventTimes;
 
-  return { ...state, TimeState, WeatherState, Timer, TimeEventTimes } as unknown as AppState;
+  return {
+    ...state,
+    TimeState,
+    WeatherState,
+    Timer,
+    TimeEventTimes,
+  } as unknown as AppState;
 };
 export type { AppState, TimeEvent };
-
