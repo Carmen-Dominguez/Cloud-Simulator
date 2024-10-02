26c709ec250a12073ec1214860b80b5e27b0a458
Carmen Dominguez <carmen.dominguez@platform45.com>
2024-10-02 13:55:14 +0200

    removed console log

--- src/index.js
+++ src/index.js
@@ -2,7 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from "./components/App";
 
-console.log('hello cloud');
-
-
 ReactDOM.render(<App />, document.getElementById('root'));
