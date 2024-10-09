69b8f489a9a42df2b124db350ce85e6d5b7d3f79
Carmen Dominguez <carmen.dominguez@platform45.com>
2024-10-09 15:37:04 +0200

    update render

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "./components/App";
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from 'react';
+import App from './components/App';
+
+const container = document.getElementById('root');
+
+const root = createRoot(container);
+
+root.render(<App />);
