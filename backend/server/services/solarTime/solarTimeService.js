a5bcc262f351261c295230b59c985b1ab6b5e57a
Carmen Dominguez <carmengdominguez94@gmail.com>
2025-05-16 13:07:44 +0200

    feat: created services

---
+++ backend/server/services/solarTime/solarTimeService.js
@@ -0,0 +1,40 @@
+const Timer = require('../../solarTime/time.js');
+
+class SolarTimeService {
+    constructor() {
+        const date = new Date();
+        const currentDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+        this.timer = new Timer(currentDate);
+    }
+
+    async initializeSolarPhases() {
+        try {
+            const response = await this.timer.getTimes();
+            const phases = response.data.results[0];
+            const cronStrings = this.timer.formatTimePhases(phases);
+            this.timer.setCromJobTimes(cronStrings);
+            return phases;
+        } catch (error) {
+            console.error('Error initializing solar phases:', error);
+            this.timer.setCromJobTimes([]);
+            throw error;
+        }
+    }
+
+    setupPhaseJobs(io, socketId) {
+        const jobCallback = (phaseInfo) => {
+            console.log('Phase change:', phaseInfo);
+            io.to(socketId).emit('phase_change', phaseInfo);
+        };
+
+        try {
+            const cronTimes = this.timer.getCronJobTimes();
+            this.timer.createNamedSolarCronJobs(cronTimes, jobCallback);
+        } catch (error) {
+            console.error('Error setting up phase jobs:', error);
+            throw error;
+        }
+    }
+}
+
+module.exports = SolarTimeService; 
---
+++ backend/server/services/weather/weatherService.js
@@ -0,0 +1,35 @@
+const Weather = require('../../weather/weather.js');
+
+class WeatherService {
+    constructor() {
+        this.weatherClient = new Weather({});
+    }
+
+    async getCurrentWeather() {
+        try {
+            const response = await this.weatherClient.getWeatherData();
+            const weatherData = response?.data?.weather || {};
+            this.weatherClient.setWeather(weatherData);
+            return this.weatherClient.getWeather();
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+            throw error;
+        }
+    }
+
+    setupWeatherUpdates(io) {
+        const cron = require('node-cron');
+        const config = require('../../config');
+
+        cron.schedule(config.WEATHER_UPDATE_INTERVAL, async () => {
+            try {
+                const weatherData = await this.getCurrentWeather();
+                io.emit('current_weather', weatherData);
+            } catch (error) {
+                console.error('Error in weather update cron job:', error);
+            }
+        });
+    }
+}
+
+module.exports = WeatherService; 
